99e3eea874f45788679a21c357dfcf8e833f0cbf
Rafael Lins <rafael@adrockers.com.br>
2025-09-29 20:18:20 -0300

    Fix missing Activity import in Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,8 @@ import {
   ChevronLeft,
   ChevronRight,
   Home,
-  FileText
+  FileText,
+  Activity
 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
