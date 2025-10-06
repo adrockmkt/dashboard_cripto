260a001507eabec503306d6cdda8f041095d37c4
Rafael Lins <rafael@adrockers.com.br>
2025-10-06 16:04:05 -0300

    Updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,7 @@
 const CACHE_NAME = 'cripto-dashboard-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/src/main.tsx',
-  '/src/index.css'
-];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
